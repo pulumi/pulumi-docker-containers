d6354420901aaa5d4e4a7afa86c722f2a5fce914
Julien Poissonnier <julien@caffeine.lu>
2024-07-25 14:49:07 +0200

    Skip azure-java

--- tests/containers_test.go
+++ tests/containers_test.go
@@ -62,6 +62,11 @@ func TestPulumiTemplateTests(t *testing.T) {
 	for _, sdk := range sdksToTest {
 		// python, typescript, ...
 		testCases = append(testCases, testCase{sdk, map[string]string{}})
+		if sdk == "java" {
+			// Don't add cloud-specific tests for Java.
+			// TODO: https://github.com/pulumi/pulumi-docker-containers/issues/210
+			continue
+		}
 		for _, cloud := range clouds {
 			// azure-python, aws-python, ...
 			testCases = append(testCases, testCase{
