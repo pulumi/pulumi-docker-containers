014843ea9bbd386c1bba212e1615533f10f44e84
Julien Poissonnier <julien@caffeine.lu>
2024-07-31 21:33:59 +0200

    Run more tests in parallel

--- tests/containers_test.go
+++ tests/containers_test.go
@@ -172,7 +172,10 @@ func TestKitchenSinkPythonVersions(t *testing.T) {
 	})
 }
 
-func TestCLIToolTests(t *testing.T) {
+func TestCloudCLIToolTests(t *testing.T) {
+	if !isKitchenSink(t) {
+		t.Skip("Only running Cloud CLI tool tests on kitchen sink")
+	}
 	t.Parallel()
 
 	t.Run("Azure CLI", func(t *testing.T) {
@@ -230,10 +233,10 @@ func TestEnvironment(t *testing.T) {
 	t.Logf("Testing image variant: %s", imageVariant)
 
 	t.Run("Python", func(t *testing.T) {
+		t.Parallel()
 		if !hasPython(t) {
 			t.Skip("Skipping test for images without python")
 		}
-		t.Parallel()
 		expected := "/usr/local/bin/python"
 		if isKitchenSink(t) {
 			expected = "/pyenv/shims/python"
@@ -249,6 +252,7 @@ func TestEnvironment(t *testing.T) {
 	})
 
 	t.Run("Node", func(t *testing.T) {
+		t.Parallel()
 		if !hasNodejs(t) {
 			t.Skip("Skipping test for images without nodejs")
 		}
@@ -256,7 +260,6 @@ func TestEnvironment(t *testing.T) {
 		if isUBI(t) || isKitchenSink(t) {
 			expected = "/usr/bin/node"
 		}
-		t.Parallel()
 		p, err := exec.LookPath("node")
 		require.NoError(t, err)
 		require.Equal(t, expected, p)
@@ -265,6 +268,7 @@ func TestEnvironment(t *testing.T) {
 	})
 
 	t.Run("PATH", func(t *testing.T) {
+		t.Parallel()
 		// Install scripts for various tools can sometimes modify PATH, usually by adding entries
 		// to ~/.bashrc. This test ensures that we notice such modifications.
 		expectedPaths := map[string]string{
