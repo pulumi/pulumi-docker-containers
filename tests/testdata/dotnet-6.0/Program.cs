9387027b93460f77c55af389a38540ec8755967a
Julien Poissonnier <julien@caffeine.lu>
2024-09-02 11:28:33 +0200

    Update to dotnet 8

---
+++ tests/testdata/dotnet-6.0/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Pulumi;
+
+return await Deployment.RunAsync(() =>
+{
+    var version = System.Environment.Version;
+    if (version.Major != 6) {
+        throw new System.Exception("Expected .NET 6 runtime, got " + version.Major);
+    }
+});
