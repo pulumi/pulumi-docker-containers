610623d1ae4b9d4b5dba350036c71200ef667ec4
Julien Poissonnier <julien@caffeine.lu>
2024-11-14 14:45:44 +0100

    Update default language versions
    As announced in https://www.pulumi.com/blog/docker-containers/ we want
    to updated the default versions:
    
    | Language Runtime | Current Version | New Version |
    | ---------------- | --------------- | ----------- |
    | .NET             | 6.0             | 8.0         |
    | Go               | 1.21            | 1.23        |
    | Node.js          | 18              | 22          |
    | Python           | 3.9             | 3.13        |
    | Java             | 17              | 21          |
    
    Fixes https://github.com/pulumi/pulumi-docker-containers/issues/321

--- .github/scripts/matrix/versions.py
+++ .github/scripts/matrix/versions.py
@@ -3,19 +3,19 @@
 unversioned = ["go", "java"]
 
 # For the versioned SDKS we have a default version and additional versions with suffixes.
-# The default version is used for the unsuffixed image `pulumi-python` and for the suffixed version `pulumi-python-3.9`.
+# The default version is used for the unsuffixed image `pulumi-python` and for the suffixed version `pulumi-python-3.13`.
 # The additional versions are used for the suffixed images `pulumi-python-3.10`, `pulumi-python-3.11`, ...
 versioned = {
     "nodejs": {
-        "default": "18",
-        "additional": ["20", "22", "23"]
+        "default": "22",
+        "additional": ["18", "20", "23"]
     },
     "python": {
-        "default": "3.9",
-        "additional": ["3.10", "3.11", "3.12", "3.13"]
+        "default": "3.13",
+        "additional": ["3.9", "3.10", "3.11", "3.12"]
     },
     "dotnet": {
-        "default": "6.0",
-        "additional": ["8.0", "9.0"]
+        "default": "8.0",
+        "additional": ["6.0", "9.0"]
     }
 }
