ed9ce004497355aa8d257d9ed7faba738c4430d1
Julien <julien@caffeine.lu>
2025-07-30 15:19:43 +0200

    Fix snyk scanning (#511)
    See
    https://github.blog/changelog/2025-07-21-code-scanning-will-stop-combining-multiple-sarif-runs-uploaded-in-the-same-sarif-file/
    
    Fixes https://github.com/pulumi/pulumi-docker-containers/issues/405

--- .github/scripts/filter-sarif.py
+++ .github/scripts/filter-sarif.py
@@ -14,6 +14,13 @@ with open("snyk.sarif") as f:
     # Keep at least one run
     runs = runs if len(runs) > 0 else [sarif["runs"][0]]
 
+    # GitHub expects each tool to only create 1 run, but Snyk splits the results
+    # across multiple runs. As a workaround, we rename the tools for each run to
+    # ensure they are unique within the file.
+    # https://github.blog/changelog/2025-07-21-code-scanning-will-stop-combining-multiple-sarif-runs-uploaded-in-the-same-sarif-file/
+    for i, run in enumerate(runs):
+        run["tool"]["driver"]["name"] += f"_{i}"
+
     sarif["runs"] = runs
 
     with open("out.sarif", "w") as out:
